3fdd94c16fe0943efbebcd11f2dcab4cf26a78a8
Luiza Santiago <lu.sntgo@gmail.com>
2022-03-22 01:16:44 -0300

    add login page layout

---
+++ src/components/Input.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+    width: 80%;
+    height: 65px;
+    background: #FFFFFF;
+    color: #111;
+    border-radius: 6px;
+    margin-bottom: 13px;
+    padding-left: 12px;
+    
+    font-size: 20px;
+    font-weight: bold;
+    font-family: 'Oswald';
+
+  &::placeholder {
+    color: #9f9f9f;
+    font-family: "Oswald";
+  }
+
+  &:disabled {
+    background: #f2f2f2;
+  }
+`;
+
+export default Input;
