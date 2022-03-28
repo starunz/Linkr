9d4712dc8d3d6c58c3f934dbc2667fd5e4eda624
luis garbelotti <luisgsgarbelotti@gmail.com>
2022-03-28 11:38:38 -0300

    fix: layout alignment on user page

--- src/pages/userPage.js/styles.js
+++ src/pages/userPage.js/styles.js
@@ -4,20 +4,37 @@ const UserContainer = styled.div`
     display: flex;    
     justify-content: center;
 
-    width: 930px;
+    width: 934px;
     max-width: 100%;
     height: 130px;
     
-    padding: 20px;
+    padding: 18px;
+    gap: 14px;
 
+    @media (max-width: 800px) {
+        width: 611px;
+        height: 100px;
+
+        gap: 18px;
+
+
+        div {
+            font-size: 35px;
+
+            display: flex;
+        }
+    }
+    
     @media (max-width: 610px) {
         width: 611px;
         height: 100px;
 
+        gap: 0;
+
         justify-content: center;
 
         div {
-            font-size: 30px;
+            font-size: 28px;
 
             display: flex;
             align-items: center;
